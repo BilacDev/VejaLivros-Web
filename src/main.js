23d3244717f9f36308878708a03b6c023a7521e6
Guilherme Rios <guilherme@justto.com.br>
2019-04-02 10:41:28 -0300

    Cria filtro

--- src/main.js
+++ src/main.js
@@ -4,12 +4,26 @@ import router from './router'
 
 // Plugin
 import './plugins/element.js'
+var VueScrollTo = require('vue-scrollto')
 
 // css
 import '@/styles/core.scss'
 import '@/styles/transitions.scss'
 
 Vue.config.productionTip = false
+Vue.use(VueScrollTo, {
+  container: "body",
+  duration: 800,
+  easing: "ease",
+  offset: 0,
+  force: true,
+  cancelable: true,
+  onStart: false,
+  onDone: false,
+  onCancel: false,
+  x: false,
+  y: true
+})
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,62 +2,83 @@
   <div class="view-home">
     <div class="view-home__cover">
       <h1 class="view-home__title">Veja Livros</h1>
-      <el-input placeholder="Busque por um livro" v-model="searchParam" @change="search" class="view-home__input">
-        <el-button slot="append" icon="el-icon-search"></el-button>
+      <el-input placeholder="Busque por um livro" v-model="searchParam" class="view-home__input">
+        <el-button slot="append" icon="el-icon-search" v-scroll-to="'#table-list'" />
       </el-input>
     </div>
-    <el-table :data="booksList" id="table-list" stripe style="width: 100%">
-      <el-table-column prop="title" label="Livro"/>
-      <el-table-column prop="author.name" label="Autor"/>
-      <el-table-column prop="created_at" label="Ano"/>
+    <el-table :data="filteredBooks" id="table-list" stripe style="width: 100%">
+      <el-table-column prop="title" label="Titulo" />
+      <el-table-column prop="author.name" label="Autor" />
+      <el-table-column prop="created_at" label="Ano" />
     </el-table>
   </div>
 </template>
 
 <script>
-import ReadBook from "@/components/ReadBook";
-import axios from "axios";
+import axios from 'axios'
 
 export default {
-  name: "home",
-  components: {
-    ReadBook
-  },
-  data() {
+  name: 'home',
+  data () {
     return {
-      searchParam: "",
-      booksList: []
-    };
+      searchParam: '',
+      booksList: [
+        {
+          title: 'Teste',
+          author: { name: 'Autor' },
+          created_at: '1999'
+        },
+        {
+          title: 'Historia de um tiozinho',
+          author: { name: 'Tiozinho do alem' },
+          created_at: '2010'
+        },
+        {
+          title: 'Eu',
+          author: { name: 'Minha mae' },
+          created_at: '2006'
+        },
+        {
+          title: '50 tons de Valentino',
+          author: { name: 'Valentino' },
+          created_at: '2019'
+        }
+      ],
+      filteredBooks: []
+    }
   },
-  mounted() {
+  mounted () {
     axios
-      .get("http://localhost:4000/api/v1/books")
+      .get('http://localhost:4000/api/v1/books')
       .then(response => response.data.data)
       .then(books => (this.booksList = books))
-      .catch(erro => console.log("Deu merda aqui", erro));
+      .catch(erro => console.log('Deu merda aqui', erro))
+  },
+  created () {
+    this.filteredBooks = this.booksList.slice(0)
+  },
+  watch: {
+    searchParam (term) {
+      this.filterSearch(term)
+    }
   },
   methods: {
-    search() {
-      console.log('entrou')
-      // Declare variables
-      const paramFormatted = this.searchParam.toUpperCase()
-      const table = document.getElementById("table-list");
-      const tr = table.getElementsByTagName("tr");
-
-      // Loop through all table rows, and hide those who don't match the search query
-      for (let i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[1];
-        if (td) {
-          if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            tr[i].style.display = "";
-          } else {
-            tr[i].style.display = "none";
-          }
-        }
+    filterSearch (term) {
+      var allBooks = this.booksList.slice(0)
+      if (term) {
+        var results = allBooks.filter(this.createFilter(term))
+        this.filteredBooks = results
+      } else {
+        this.filteredBooks = allBooks
+      }
+    },
+    createFilter (term) {
+      return (occurrence) => {
+        return (occurrence.title.toLowerCase().includes(term.toLowerCase())) || (occurrence.created_at.toLowerCase().includes(term.toLowerCase())) || (occurrence.author.name.toLowerCase().includes(term.toLowerCase()))
       }
     }
   }
-};
+}
 </script>
 
 <style lang="scss">
@@ -73,6 +94,7 @@ export default {
     justify-content: center;
     align-items: center;
     flex-direction: column;
+    filter: grayscale(0.3)
   }
   &__title {
     color: white;
